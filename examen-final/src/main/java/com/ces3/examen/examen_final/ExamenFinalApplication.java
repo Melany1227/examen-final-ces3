953e2c14f50fb67f10b9abae745e784478172fde
Melany <melanyjsuarez@gmail.com>
2025-06-18 18:54:33 -0500

    Initial project setup and dependencies

---
+++ examen-final/src/main/java/com/ces3/examen/examen_final/ExamenFinalApplication.java
@@ -0,0 +1,19 @@
+package com.ces3.examen.examen_final;
+
+import com.ces3.examen.examen_final.modelo.Teacher;
+import com.ces3.examen.examen_final.repositorio.TeacherRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ExamenFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenFinalApplication.class, args);
+	}
+
+}
---
+++ examen-final/src/test/java/com/ces3/examen/examen_final/ExamenFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ces3.examen.examen_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
