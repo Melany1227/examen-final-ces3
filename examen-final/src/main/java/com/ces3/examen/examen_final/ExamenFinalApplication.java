c35e0710a5f73b043d94ec67d362a9b557a4241e
Melany <melanyjsuarez@gmail.com>
2025-06-18 19:11:04 -0500

    Add CommandLineRunner and console output

--- examen-final/src/main/java/com/ces3/examen/examen_final/ExamenFinalApplication.java
+++ examen-final/src/main/java/com/ces3/examen/examen_final/ExamenFinalApplication.java
@@ -16,4 +16,21 @@ public class ExamenFinalApplication {
 		SpringApplication.run(ExamenFinalApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner initDatabase(TeacherRepository repo) {
+		return args -> {
+			Teacher t1 = new Teacher("Laura", "Gómez", "Matemáticas", 3000.0, "laura@correo.com");
+			Teacher t2 = new Teacher("Carlos", "Ruiz", "Física", 3200.0, "carlos@correo.com");
+			Teacher t3 = new Teacher("Ana", "Martínez", "Química", 3100.0, "ana@correo.com");
+
+			repo.save(t1);
+			repo.save(t2);
+			repo.save(t3);
+
+			for (Teacher t : repo.findAll()) {
+				System.out.println(t.toString() + " - Salario anual: $" + t.calcularSalarioAnual());
+			}
+		};
+	}
+
 }
